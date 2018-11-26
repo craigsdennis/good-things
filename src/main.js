1d67a7008bfcfa04769e445360e38be05197e316
craigsdennis <craig@teamtreehouse.com>
2018-11-25 20:42:01 -0800

    Initial swing at an async syncClient

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import syncClient from './syncClient'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  syncClient,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/syncClient.js
@@ -0,0 +1,16 @@
+import SyncClient  from 'twilio-sync'
+
+// TODO: Export
+const SYNC_TOKEN_URI = 'https://quartz-ant-5504.twil.io/sync-token';
+
+async function getToken() {
+    // TODO: check here if we got a token already
+    const response = await fetch(SYNC_TOKEN_URI);
+    const json = await response.json();
+    return json.token;
+}
+
+export default async function getSyncClient() {
+    const token = await getToken();
+    return new SyncClient(token);
+}
